f7d45f673bd4d30c0de01f60101edb0c3fd59952
ch-pat <patryk.choma@gmail.com>
2021-12-02 10:28:00 +0100

    aoc 2

---
+++ 2/solve.py
@@ -0,0 +1,27 @@
+input_file = "2/input"
+
+with open(input_file, "r") as f:
+    data = f.read().splitlines()
+data = [tuple(x.split()) for x in data]
+
+x, y = 0, 0
+
+for movement in data:
+    move, amount = movement[0], int(movement[1])
+    if move == "down": y += amount
+    if move == "up": y -= amount
+    if move == "forward": x += amount
+
+print(x * y)
+
+x, y, aim = 0, 0, 0
+
+for movement in data:
+    move, amount = movement[0], int(movement[1])
+    if move == "down": aim += amount
+    if move == "up": aim -= amount
+    if move == "forward": 
+        x += amount
+        y += aim * amount
+
+print(x * y)
