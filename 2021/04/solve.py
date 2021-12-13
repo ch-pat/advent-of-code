a6b3ef5e3344b8687192ef249c1b5cbe7e4a3ef8
ch-pat <patryk.choma@gmail.com>
2021-12-13 09:51:52 +0100

    moved into subfolder and renamed repo to be year-agnostic

---
+++ 2021/04/solve.py
@@ -0,0 +1,86 @@
+input_file = "2021/04/input"
+
+with open(input_file, "r") as f:
+    data = f.read().splitlines()
+
+class Board():
+    winning_combinations = {
+        frozenset([0, 1, 2, 3, 4]), frozenset([5, 6, 7, 8, 9]), frozenset([10, 11, 12, 13, 14]),
+        frozenset([15, 16, 17, 18, 19]), frozenset([20, 21, 22, 23, 24]), frozenset([0, 5, 10, 15, 20]), 
+        frozenset([1, 6, 11, 16, 21]), frozenset([2, 7, 12, 17, 22]), frozenset([3, 8, 13, 18, 23]), 
+        frozenset([4, 9, 14, 19, 24])
+    }
+    def __init__(self, board, idd):
+        self.id = idd
+        self.board = tuple((x for line in board for x in line))
+        self.number_set = set([])
+        for line in board:
+            for el in line:
+                self.number_set.add(el)
+        self.called = set([])
+        self.has_won = False
+
+    def __repr__(self):
+        res = f"Board number {self.id}:\n"
+        for i, n in enumerate(self.board):
+            res += str(n) + ' '
+            if i % 5 == 4:
+                res += '\n'
+        return res + '\n'
+
+    def call_number(self, n):
+        if n not in self.number_set:
+            return
+        self.called.add(self.board.index(n))
+
+    def is_in_winning_state(self):
+        return any((combo.issubset(self.called) for combo in self.winning_combinations))
+
+    def calculate_score(self, last_called_number):
+        uncalled = [self.board[i] for i in range(len(self.board)) if i not in self.called]
+        score = sum(uncalled) * last_called_number
+        return score
+
+
+bingo_numbers = [int(x) for x in data[0].split(",")]
+
+bingo_boards_wip = data[2:] + [""]
+
+current_board = []
+bingo_boards = []
+counter = 0
+for i, line in enumerate(bingo_boards_wip):
+    if i % 6 != 5:
+        current_board += [[int(x) for x in line.split()]]
+    else:
+        bingo_boards += [Board(current_board, counter)]
+        current_board = []
+        counter += 1
+end = False
+for number in bingo_numbers:
+    for board in bingo_boards:
+        board.call_number(number)
+        if board.is_in_winning_state():
+            print(board)
+            print(board.called)
+            print(board.calculate_score(number))
+            end = True
+            break
+    if end:
+        break
+
+### Part two
+last_winner = None
+last_called_number = None
+for number in bingo_numbers:
+    for board in bingo_boards:
+        if not board.has_won:
+            board.call_number(number)
+            if board.is_in_winning_state():
+                board.has_won = True
+                last_winner = board
+                last_called_number = number
+
+print(last_winner)
+print(last_winner.called)
+print(last_winner.calculate_score(last_called_number))
---
+++ 2021/08/solve.py
@@ -0,0 +1,89 @@
+input_file = "2021/08/input"
+
+with open(input_file, "r") as f:
+    data = f.read().splitlines()
+
+inputs, outputs = [], []
+
+for line in data:
+    i, o = line.split(' | ')
+    i = i.split()
+    o = o.split()
+    inputs += [i]
+    outputs += [o]
+
+
+unique_lengths = (2, 4, 3, 7)
+count = 0
+for o in outputs:
+    for element in o:
+        if len(element) in unique_lengths:
+            count += 1
+print(count)
+
+### Part Two
+
+def get_obvious_numbers(numbers):
+    numbers_dict = {}
+    numbers_dict[0] = None
+    numbers_dict[6] = None
+    numbers_dict[9] = None
+    numbers_dict[2] = None
+    numbers_dict[5] = None
+    numbers_dict[3] = None
+    for n in numbers:
+        if len(n) == 2:
+            numbers_dict[1] = set(n)
+        elif len(n) == 4:
+            numbers_dict[4] = set(n)
+        elif len(n) == 3:
+            numbers_dict[7] = set(n)
+        elif len(n) == 7:
+            numbers_dict[8] = set(n)
+    return numbers_dict
+
+def get_ambiguous_numbers(numbers_dict, numbers):
+    eight = numbers_dict[8]
+    four = numbers_dict[4]
+    one = numbers_dict[1]
+    two = None
+    tleft_bright = None
+    top_bottom_bleft: set = eight - four
+    center_tleft: set = four - one
+    done = []
+    while not all((numbers_dict[i] for i in range(10))):
+        for n in numbers:
+            sectors = set(n)
+            if len(n) == 5:
+                #can be 2, 5, 3
+                # Recognize two
+                if top_bottom_bleft.issubset(sectors):
+                    numbers_dict[2] = sectors
+                    two = sectors
+                    tleft_bright = eight - two
+                if tleft_bright:
+                    if tleft_bright.issubset(sectors):
+                        numbers_dict[5] = sectors
+                if numbers_dict[5] and numbers_dict[2] and numbers_dict[5] != sectors and numbers_dict[2] != sectors:
+                    numbers_dict[3] = sectors
+            elif len(n) == 6:
+                #can be 0, 6, 9
+                if not top_bottom_bleft.issubset(sectors):
+                    numbers_dict[9] = sectors
+                if numbers_dict[9] and numbers_dict[9] != sectors and center_tleft.issubset(sectors):
+                    numbers_dict[6] = sectors
+                if numbers_dict[6] and numbers_dict[9] and numbers_dict[6] != sectors and numbers_dict[9] != sectors:
+                    numbers_dict[0] = sectors
+    return numbers_dict
+
+displayed_numbers = []
+for i, o in zip(inputs, outputs):
+    numbers_dict = get_obvious_numbers(i)
+    numbers_dict = get_ambiguous_numbers(numbers_dict, i)
+    inverse_dict = {frozenset(v):k for k, v in numbers_dict.items()}
+    current_number = 0
+    for power, n in enumerate(o):
+        current_number += int(inverse_dict[frozenset(n)]) * 10 ** (3 - power)
+    displayed_numbers += [current_number]
+print(displayed_numbers)
+print(sum(displayed_numbers))
